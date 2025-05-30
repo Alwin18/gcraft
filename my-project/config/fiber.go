3cd4314d3ba32dae9bdce3bc7f2410f0392e3f31
AlwinL <alwinbezarius@gmail.com>
2025-05-30 10:43:31 +0700

    Fix Render Template

--- my-project/config/fiber.go
+++
@@ -1,9 +0,0 @@
-package config
-
-import "github.com/gofiber/fiber/v2"
-
-func NewFiber(cfg *Config) *fiber.App {
-	return fiber.New(fiber.Config{
-		Network: fiber.NetworkTCP,
-	})
-}
--- my-project/internal/service/user-service/response-service.go
+++
@@ -1,7 +0,0 @@
-package userservice
-
-type GetListUsers struct {
-	ID       int64  `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-}
